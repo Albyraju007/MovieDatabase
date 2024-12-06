667e11db26ecaa216e7a849209279dfb4a0bd98b
AlbyRaju123 <albyraju0@gmail.com>
2024-12-06 22:27:23 +0530

    initial code template has been added

---
+++ src/main/java/com/moviedatabase/MovieDataBaseApplication.java
@@ -0,0 +1,13 @@
+package com.moviedatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieDataBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieDataBaseApplication.class, args);
+	}
+
+}
